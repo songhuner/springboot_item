3259f270cc7a5d1a8ee883caa83883e3978acd11
hjh <123456@qq.com>
2020-12-19 00:43:28 +0800

    新增日志功能

--- src/main/java/com/hang/springbootdemon/service/impl/PersonSvcImpl.java
+++ src/main/java/com/hang/springbootdemon/service/impl/PersonSvcImpl.java
@@ -3,6 +3,7 @@ package com.hang.springbootdemon.service.impl;
 import com.hang.springbootdemon.entity.Person;
 import com.hang.springbootdemon.mapper.PersonMapper;
 import com.hang.springbootdemon.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,14 @@ import org.springframework.stereotype.Service;
  * @date 2020/9/8
  */
 @Service
+@Slf4j
 public class PersonSvcImpl implements PersonService {
     @Autowired
     private PersonMapper personMapper;
     @Override
     public Person getPersonByid(Long personId) {
+        log.info("personId是："+personId);
+        log.error("错误日志测试");
         return personMapper.findPersonById(personId);
     }
 }
